e8dde0910a0db1c68ef2108afae35ef6e16b2f72
cLohan <82850047+cLohan@users.noreply.github.com>
2026-09-10 22:20:28 -0300

    Build normal preview without Vulkan transport hooks

--- src/neural/build_config.h
+++ src/neural/build_config.h
@@ -3,5 +3,5 @@
 // The preview/master branch builds D3D11/D3D12; preview/vulkan also enables
 // the Vulkan transport and its device-creation hook. The neural core is shared.
 #ifndef DLSS5_WITH_VULKAN
-#define DLSS5_WITH_VULKAN 1
+#define DLSS5_WITH_VULKAN 0
 #endif
